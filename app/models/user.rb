e52cdd275931b7d8447c3af0efaefa18ccfd31ae
Victor <stasekvictordev@gmail.com>
2019-02-13 01:06:47 -0500

    more user e-mail validation. verifying no duplicates @ model AND db level (added index)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    before_save { email.downcase! } # = before_save { self.email = email.downcase }
+
+    # validation ----------
     validates :name,  presence: true, length: { maximum: 50 }
     
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
