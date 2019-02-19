ed7d75ab75c14bec82cf645baea8d9b9529f83b1
Victor Stasek <stasekvictor@gmail.com>
2019-02-19 15:58:25 -0500

    add user log_out functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
   private
 
     def user_params
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,20 @@
 module SessionsHelper
-    
+
   # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
   end
 
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   # Returns the current logged-in user (if any).
   def current_user
     if session[:user_id]
+      # if @current_user hasn't already been set, sets @current_user
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
