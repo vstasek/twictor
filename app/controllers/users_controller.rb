1583e959915409d253995b70e0e0307b502a6195
Victor <stasekvictordev@gmail.com>
2019-02-20 22:19:03 -0500

    add update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,15 @@ class UsersController < ApplicationController
   def destroy
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
