b4d42ebce9180ee72ba9d786453df2a9bfd15d0f
Victor Stasek <stasekvictor@gmail.com>
2019-02-27 16:05:34 -0500

    implement friendly forwarding

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -4,10 +4,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:joe)
-    log_in_as(@user)
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name:  "",
@@ -20,8 +20,27 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    assert_nil session[:forwarding_url] # won't redirect to edit user on subsequent login
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:  name,
