15adc8c91c790f5da9627cbda57511a2b6e44775
Victor <stasekvictordev@gmail.com>
2019-03-03 22:07:24 -0500

    admin user functionality

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:joe)
-    @other_user = users(:victor)
+    @user = users(:victor)
+    @other_user = users(:joe)
   end
 
   test "should get new" do
@@ -28,5 +28,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should redirect index when not logged in" do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              "password",
+                                            password_confirmation: "password",
+                                            admin: true } }
+    assert_not @other_user.admin?
   end
 end
