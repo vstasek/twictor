3080971b3bcaae48617ce641c06650f62d7d4a15
Victor <stasekvictordev@gmail.com>
2019-03-10 00:34:34 -0500

    fixes syntax error in account_activations resources declaration

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
-  resources :account_activations, only [:edit]
+  resources :account_activations, only: [:edit]
 end
