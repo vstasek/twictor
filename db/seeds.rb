3de57756926bd16001a293f51ddffa927fa9ee0d
Victor Stasek <stasekvictor@gmail.com>
2019-03-14 09:03:02 -0400

    fixes syntax error, missing commas

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ User.create!(name:  "Example User",
   email: "example@railstutorial.org",
   password:              "foobar",
   password_confirmation: "foobar",
-  admin: true
+  admin: true,
   activated: true,
   activated_at: Time.zone.now)
 
@@ -13,7 +13,7 @@ password = "password"
 User.create!(name:  name,
     email: email,
     password:              password,
-    password_confirmation: password
+    password_confirmation: password,
     activated: true,
     activated_at: Time.zone.now)
 end
