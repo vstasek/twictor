086bf069f2712497953fd6323a74fc884243b994
Victor Stasek <stasekvictor@gmail.com>
2019-03-14 14:25:23 -0400

    turns email.downcase before_save into a private method call

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     attr_accessor :remember_token, :activation_token
     before_create :create_activation_digest
-    before_save { email.downcase! } # = before_save { self.email = email.downcase }
+    before_save   :downcase_email
 
     # validation ----------
     validates :name,  presence: true, length: { maximum: 50 }
@@ -43,6 +43,10 @@ class User < ApplicationRecord
   end
 
   private
+    def downcase_email
+      { email.downcase! } # = before_save { self.email = email.downcase }
+    end
+
     # Creates and assigns the activation token and digest
     def create_activation_digest
       self.activation_token   = User.new_token
