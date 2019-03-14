174e158b9725a17fe6c8896ae2d576710f1e3d20
Victor Stasek <stasekvictor@gmail.com>
2019-03-14 14:24:02 -0400

    adds create_activation_digest method that + before_create method call

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :activation_token
+    before_create :create_activation_digest
     before_save { email.downcase! } # = before_save { self.email = email.downcase }
 
     # validation ----------
@@ -41,4 +42,10 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  private
+    # Creates and assigns the activation token and digest
+    def create_activation_digest
+      self.activation_token   = User.new_token
+      self.activation_digest  = User.digest(activation_token)
+    end
 end
